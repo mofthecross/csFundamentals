d49af6d211511b1252271b84b106d6e11b1c87ac
mofthecross <mofthecross@gmail.com>
2017-06-20 16:56:41 -0700

    Add bitFlip problem

---
+++ dynamicProgramming/bitFlip.js
@@ -0,0 +1,14 @@
+/*
+Given an array of binary values (0 and 1) and N number of flips (convert a 0 to a 1),
+  determine the maximum number of consecutive 1's that can be made.
+
+  Input: An Array of 1's and 0's, and an Integer N denoting the number of flips
+  Output: Integer
+
+  Example: bitFlip([0,1,1,1,0,1,0,1,0,0], 2)
+  Result: 7
+*/
+
+function maxNumConsecutiveOnesWithFlips(n){
+  // YOUR WORK HERE
+}
